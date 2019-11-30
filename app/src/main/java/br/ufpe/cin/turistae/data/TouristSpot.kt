6c3d9508c7e27ec7c1ccd3b533cefa5da54fe0ab
wilquerlima <wtl@cin.ufpe.br>
2019-11-30 18:06:40 -0300

    Creating item for recyclerView

--- app/src/main/java/br/ufpe/cin/turistae/data/TouristSpot.kt
+++ app/src/main/java/br/ufpe/cin/turistae/data/TouristSpot.kt
@@ -1,3 +1,8 @@
 package br.ufpe.cin.turistae.data
 
-data class TouristSpot(val name: String, val address: Address)
+data class TouristSpot(
+    val name: String,
+    val address: Address,
+    val description: String,
+    val photoUrl: String
+)
